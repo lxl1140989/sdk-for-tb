511c4dae9d366339b1c13bf26a9dc93ddd7517c0
lxl <xiaolong3188@163.com>
2016-08-08 09:21:39 +0800

    update for set wifi

--- files/sbin/init_network.sh
+++ files/sbin/init_network.sh
@@ -19,7 +19,7 @@ fi
 if [ "$ap_encrypt" = "none" ]; then
 	if [ "$wifi_mode" = "2g" ]; then
 #		uaputl sys_cfg_ssid "$ap_ssid"
-		iwpriv uap0 apcfg "ASCII_CMD=AP_CFG,SSID=\"$ap_ssid\",SEC=open"
+		iwpriv uap0 apcfg "ASCII_CMD=AP_CFG,SSID=$ap_ssid,SEC=open"
 	else
 		dhd_helper ssid "$ap_ssid" hidden n chan $ch5g amode open emode none
 	fi
@@ -27,7 +27,7 @@ if [ "$ap_encrypt" = "none" ]; then
 else
 	if [ "$wifi_mode" = "2g" ]; then
 #		dhd_helper ssid "$ap_ssid" bgnmode bgn chan $ch2g amode wpa2psk emode aes key "$ap_key"
-		iwpriv uap0 apcfg "ASCII_CMD=AP_CFG,SSID=\"$ap_ssid\",SEC=WPA2-PSK,KEY=$ap_key"
+		iwpriv uap0 apcfg "ASCII_CMD=AP_CFG,SSID=$ap_ssid,SEC=WPA2-PSK,KEY=$ap_key"
 	else
 		dhd_helper ssid "$ap_ssid" chan $ch5g amode wpa2psk emode aes key "$ap_key"
 	fi
